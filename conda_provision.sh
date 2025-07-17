0a7c5995e1fa914efa35ffd9b6a9bfbfce1819a9
BrindanYasodaran <125691237+BrindanYasodaran@users.noreply.github.com>
2025-07-17 11:05:55 +0100

    Update conda_provision.sh

--- conda_provision.sh
+++ conda_provision.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -eo pipefail
 
+# Accept Anaconda Terms for necessary channels
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+# Your existing configuration to maybe switch channels or continue...
 conda config --remove channels defaults
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 
 env >> /etc/environment
-
-echo "Conda channels configured"
+echo "Conda channels and ToS acceptance configured"
